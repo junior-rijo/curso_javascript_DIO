7fe060bb02de1c3080cfbf3ee7ee9e284bec11ca
Alberino Junior <alberino.rijo@outlook.com>
2020-01-09 17:31:28 -0300

    testes iniciais

---
+++ script.js
@@ -0,0 +1,10 @@
+var nome =  'Junior Rijo';
+var idade = 29
+var idade2 = 10 
+var frase = 'Japão'
+//alert('Seja Bem Vindo !' + nome + "voce tem" + idade); usando alert e console, var e as strings 
+//alert(idade + idade2)
+
+console.log(nome);
+console.log(frase.replace("Japão", "Brasil"));//trocando os Países..
+console.log(frase.toLowerCase());//tudo em minisculo
