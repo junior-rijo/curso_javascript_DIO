ab22a5dd70b395c1328c3668a088ac782bd2f3b9
Alberino Junior <alberino.rijo@outlook.com>
2020-01-10 15:14:36 -0300

    arrays, diconários e list conceitos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var nome =  'Junior Rijo';
+/*var nome =  'Junior Rijo';
 var idade = 29
 var idade2 = 10 
 var frase = 'Japão'
@@ -7,4 +7,19 @@ var frase = 'Japão'
 
 console.log(nome);
 console.log(frase.replace("Japão", "Brasil"));//trocando os Países..
-console.log(frase.toLowerCase());//tudo em minisculo
+console.log(frase.toLowerCase());//tudo em minusculo*/
+
+//array list//
+
+/*var lista = ['maça', 'abacaxi', 'limão'];
+lista.push("uva")
+
+console.log(lista);*/
+
+//dicionário//
+/*var fruta = {nome: 'maça', cor:'vermelho'};
+console.log(fruta.nome);*/
+//lista de dicionários
+var frutas = [{nome: 'maça', cor:'vermelha'}, {nome:"limão", cor:"verde"}]
+console.log(frutas);
+alert([1], nome);
